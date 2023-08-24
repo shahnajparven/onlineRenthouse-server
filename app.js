2745a6eaef965301739fe19ac53d600feb740da8
shahnaj parven <shahnajritu21@gmail.com>
2023-08-24 22:10:25 +0600

    change

--- app.js
+++ app.js
@@ -34,11 +34,6 @@ app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
 
-// app.use(express.static(path.join(__dirname,"../frontend/build")));
-// app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "../frontend/build/index.html"));
-//   });
-
 //middleware for error
 app.use(errorMidleware);
 
